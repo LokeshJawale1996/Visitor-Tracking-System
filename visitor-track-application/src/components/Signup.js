a1ee746c90722b374fe1dcc3acc37e8f7c411c42
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2023-08-03 13:00:23 +0530

    signup validations work

--- visitor-track-application/src/components/Signup.js
+++ visitor-track-application/src/components/Signup.js
@@ -1,16 +1,26 @@
+import React, { useState } from "react";
 
-import React, { useState } from 'react';
-
-const Signup= () => {
+const Signup = () => {
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    dob: '',
-    role: '',
-    apartmentName: '',
-    password: '',
-    confirmPassword: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    dob: "",
+    role: "",
+    apartmentName: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [validations, setValidations] = useState({
+    firstName: true,
+    lastName: true,
+    email: true,
+    password: true,
+    confirmPassword: true,
+    age: true,
+    role: true,
+    apartmentName: true,
   });
 
   const handleInputChange = (event) => {
@@ -18,11 +28,90 @@ const Signup= () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSignUp = () => {
-    localStorage.setItem('signupData', JSON.stringify(formData));
-    alert('Sign up successful! Data saved in localStorage.');
-    console.log(localStorage.getItem('signupData'))
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+    if (name === "email") {
+      const isValidEmail = event.target.validity.valid;
+      setValidations({ ...validations, [name]: isValidEmail });
+    } else if (name === "password" || name === "confirmPassword") {
+      const isValidPassword =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+          value
+        );
+      setValidations({ ...validations, [name]: isValidPassword });
+    } else if (name === "dob") {
+      // Calculate age based on Date of Birth (dob)
+      const dobDate = new Date(value);
+      const currentDate = new Date();
+      const age = currentDate.getFullYear() - dobDate.getFullYear();
+      const isAbove18 = age >= 18;
+      setValidations({ ...validations, age: isAbove18 });
+    } else {
+      const isValid = /^[a-zA-Z]+$/.test(value);
+      setValidations({ ...validations, [name]: isValid });
+    }
   };
+  const handleSignUp = () => {
+    // Check if all validations are true and passwords match
+    if (
+      validations.firstName &&
+      validations.lastName &&
+      validations.email &&
+      validations.apartmentName &&
+      validations.password &&
+      validations.confirmPassword &&
+      validations.age &&
+      formData.password === formData.confirmPassword &&
+      formData.role
+    ) {
+      localStorage.setItem("signupData", JSON.stringify(formData));
+
+      alert("Sign up successful! Data saved in Localstorage.");
+    } else {
+      alert("Please enter valid information and ensure passwords match.");
+    }
+
+    console.log(localStorage.getItem("signupData"));
+  };
+
+  const firstNameOutline = validations.firstName
+    ? "border-green-500 border-2"
+    : "border-red-500 border-2";
+  const lastNameOutline = validations.lastName
+    ? "border-green-500 border-2"
+    : "border-red-500 border-2";
+  const emailOutline = validations.email
+    ? "border-green-500 border-2"
+    : "border-red-500 border-2";
+  const passwordOutline =
+    validations.password && formData.password === formData.confirmPassword
+      ? "border-green-500 border-2"
+      : "border-red-500 border-2";
+  const confirmPasswordOutline =
+    validations.confirmPassword &&
+    formData.password === formData.confirmPassword
+      ? "border-green-500 border-2"
+      : "border-red-500 border-2";
+  const ageOutline = validations.age
+    ? "border-green-500 border-2"
+    : "border-red-500 border-2";
+  const roleOutline = validations.role
+    ? "border-green-500 border-2"
+    : "border-red-500 border-2";
+  const apartmentNameOutline = validations.apartmentName
+    ? "border-green-500 border-2"
+    : "border-red-500 border-2";
+
+  const isSignupDisabled =
+    !validations.firstName ||
+    !validations.lastName ||
+    !validations.email ||
+    !validations.apartmentName ||
+    !validations.password ||
+    !validations.confirmPassword ||
+    !validations.age ||
+    formData.password !== formData.confirmPassword ||
+    !formData.role;
 
   return (
     <div className="w-96 mx-auto p-4 border rounded shadow">
@@ -34,7 +123,8 @@ const Signup= () => {
           placeholder="First Name"
           value={formData.firstName}
           onChange={handleInputChange}
-          className="w-40 mr-2 p-2 border rounded"
+          onBlur={handleBlur}
+          className={`w-40 mr-2 p-2 border rounded ${firstNameOutline}`}
         />
         <input
           type="text"
@@ -42,7 +132,8 @@ const Signup= () => {
           placeholder="Last Name"
           value={formData.lastName}
           onChange={handleInputChange}
-          className="w-40 p-2 border rounded"
+          onBlur={handleBlur}
+          className={`w-40 mr-2 p-2 border rounded ${lastNameOutline}`}
         />
       </div>
       <div className="mb-4">
@@ -52,7 +143,8 @@ const Signup= () => {
           placeholder="Email Address"
           value={formData.email}
           onChange={handleInputChange}
-          className="w-full p-2 border rounded"
+          onBlur={handleBlur}
+          className={`w-full p-2 border rounded ${emailOutline}`}
         />
       </div>
       <div className="mb-4">
@@ -61,7 +153,7 @@ const Signup= () => {
           name="dob"
           value={formData.dob}
           onChange={handleInputChange}
-          className="w-full p-2 border rounded"
+          className={`w-full p-2 border rounded ${ageOutline}`}
         />
       </div>
       <div className="mb-4">
@@ -69,7 +161,8 @@ const Signup= () => {
           name="role"
           value={formData.role}
           onChange={handleInputChange}
-          className="w-full p-2 border rounded"
+          onBlur={handleBlur}
+          className={`w-full p-2 border rounded ${roleOutline}`}
         >
           <option value="">Select Role</option>
           <option value="security">Security</option>
@@ -77,18 +170,19 @@ const Signup= () => {
           <option value="apartmentHead">Apartment Head</option>
         </select>
       </div>
-      {formData.role === 'apartmentHead' && (
-        <div className="mb-4">
-          <input
-            type="text"
-            name="apartmentName"
-            placeholder="Apartment Name"
-            value={formData.apartmentName}
-            onChange={handleInputChange}
-            className="w-full p-2 border rounded"
-          />
-        </div>
-      )}
+
+      <div className="mb-4">
+        <input
+          type="text"
+          name="apartmentName"
+          placeholder="Apartment Name"
+          value={formData.apartmentName}
+          onChange={handleInputChange}
+          onBlur={handleBlur}
+          className={`w-full p-2 border rounded ${apartmentNameOutline}`}
+        />
+      </div>
+
       <div className="mb-4">
         <input
           type="password"
@@ -96,7 +190,7 @@ const Signup= () => {
           placeholder="Password"
           value={formData.password}
           onChange={handleInputChange}
-          className="w-full p-2 border rounded"
+          className={`w-full p-2 border rounded ${passwordOutline}`}
         />
       </div>
       <div className="mb-4">
@@ -106,12 +200,15 @@ const Signup= () => {
           placeholder="Confirm Password"
           value={formData.confirmPassword}
           onChange={handleInputChange}
-          className="w-full p-2 border rounded"
+          className={`w-full p-2 border rounded ${confirmPasswordOutline}`}
         />
       </div>
       <button
         onClick={handleSignUp}
-        className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+        className={`w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 ${
+          isSignupDisabled ? "opacity-50 cursor-not-allowed" : ""
+        }`}
+        disabled={isSignupDisabled}
       >
         Sign Up
       </button>
